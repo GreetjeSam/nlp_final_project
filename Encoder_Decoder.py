fixed validation loss calculation

Encoder_Decoder.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class EngEncoder(nn.Module):
-    def __init__(self, eng_vocab_size: int, hidden_state_size: int, dropout_probability: float = 0.1):
+    def __init__(self, eng_vocab_size: int, hidden_state_size: int, longest_sentence, dropout_probability: float = 0.2):
         super(EngEncoder, self).__init__()
         self.hidden_size = hidden_state_size
 
@@ -13,7 +13,12 @@ class EngEncoder(nn.Module):
         self.embedding = nn.Embedding(eng_vocab_size,hidden_state_size)
 
         #using the GRU RNN to the embedded input which the size is the hidden_state_size
-        self.encoderrnn = nn.GRU(hidden_state_size,hidden_state_size,batch_first=True)
+        self.encoderrnn = nn.LSTM(hidden_state_size,hidden_state_size,batch_first=True)
+        self.encoderrnn2 = nn.LSTM(hidden_state_size,hidden_state_size,batch_first=True)
+
+        #self.encoderrnn2 = nn.GRU(hidden_state_size,hidden_state_size,batch_first=True)
+        self.batch_norm = nn.BatchNorm1d(longest_sentence)
+
 
         #using the dropout to zero the elements in the tensor with the dropout_probability
         self.dropout = nn.Dropout(dropout_probability)
@@ -24,11 +29,16 @@ class EngEncoder(nn.Module):
         embedded = self.embedding(input)
 
         #zero some elements in the input
-        dropout = self.dropout(embedded)
+        #dropout = self.dropout(embedded)
 
         #get the output tensor and the hiddenstate
-        encoder_output, hidden_state = self.encoderrnn(dropout)
-        return encoder_output, hidden_state
+        encoder_output1, (hidden_state1, cell_state1) = self.encoderrnn(embedded)
+        encoder_output2, (hidden_state2, cell_state2) = self.encoderrnn2(encoder_output1)
+
+        encoder_output2 = self.batch_norm(encoder_output2)
+        encoder_output2 = self.dropout(encoder_output2)
+
+        return encoder_output2, hidden_state1
     
 
 class Attention(nn.Module):
@@ -56,7 +66,7 @@ class Attention(nn.Module):
 
 
 class NlDecoder(nn.Module):
-    def __init__(self, hidden_state_size: int, nl_vocab_size: int, vocab_nl, longest_sentence: int, dropout_probability: float =0.05):
+    def __init__(self, hidden_state_size: int, nl_vocab_size: int, vocab_nl, longest_sentence: int, dropout_probability: float = 0.2):
         super(NlDecoder, self).__init__()
 
         #using the embedding module to get word embeddings 
@@ -67,11 +77,14 @@ class NlDecoder(nn.Module):
 
         #using the GRU RNN to the embedded input which the size is double the hidden_state_size
         self.decoderrnn = nn.GRU(2 * hidden_state_size, hidden_state_size, batch_first=True)
+        #self.decoderrnn2 = nn.GRU(hidden_state_size, hidden_state_size, batch_first=True)
 
         #using the output layer to get the output
         self.output_layer = nn.Linear(hidden_state_size, nl_vocab_size)
 
          #using the dropout to zero the elements in the tensor with the dropout_probability
+        self.batch_norm = nn.BatchNorm1d(1)
+
         self.dropout = nn.Dropout(dropout_probability)
         self.vocab_nl = vocab_nl
         self.longest_sentence = longest_sentence
@@ -130,21 +143,26 @@ class NlDecoder(nn.Module):
 
         #embed the input and dropout 
         embedded =  self.embedding(input)
-        dropout = self.dropout(embedded)
+        #dropout = self.dropout(embedded)
 
         query = hidden.permute(1, 0, 2)
 
         context, attn_weights = self.attention(query, encoder_outputs)
 
         #concatenate the dropouted and embedded input and the context vector in two dimensions
-        input_gru = torch.cat((dropout, context), dim=2)
+        input_gru = torch.cat((embedded, context), dim=2)
 
-        output, hidden_state = self.decoderrnn(input_gru, hidden)
-        
+        output1, hidden_state1 = self.decoderrnn(input_gru, hidden)
+        #output2, hidden_state2 = self.decoderrnn2(output1, hidden_state1)
+
+        output2 = self.batch_norm(output1)
+        output2 = self.dropout(output2)
+
+
         #let the output go through the output layer
-        output = self.output_layer(output)
+        output = self.output_layer(output2)
         
-        return output, hidden_state, attn_weights
+        return output, hidden_state1, attn_weights
 
     
 

Evaluation.py
@@ -23,12 +23,12 @@ class Evaluation():
 
             encoder_outputs, encoder_hidden = self.encoder(input_tensor)
             decoder_outputs, decoder_hidden, decoder_attn = self.decoder(encoder_outputs, encoder_hidden)
-            _, topi = torch.topk(decoder_outputs,2)
+            _, topi = torch.topk(decoder_outputs,1)
             decoded_ids = topi.squeeze()
             #print(decoded_ids[:,1])
 
             decoded_words = []
-            for idx in decoded_ids[:,1]:
+            for idx in decoded_ids:
                 if idx.item() == self.EOS_token:
                     decoded_words.append('<EOS>')
                     break

Training.py
@@ -73,8 +73,8 @@ class Training():
         plot_train_losses = []
         plot_val_losses = []
 
-        encoder_optimizer = optimizer(mod.parameters(encoder), lr=learning_rate, weight_decay=1e-3)
-        decoder_optimizer = optimizer(mod.parameters(decoder), lr=learning_rate, weight_decay=1e-3)
+        encoder_optimizer = optimizer(mod.parameters(encoder), lr=learning_rate, weight_decay=1e-6)
+        decoder_optimizer = optimizer(mod.parameters(decoder), lr=learning_rate, weight_decay=1e-6)
         
         # Using the cross entropy loss
         criterion = nn.CrossEntropyLoss()
@@ -93,9 +93,9 @@ class Training():
                     vloss = criterion(decoder_outputs.view(-1, decoder_outputs.size(-1)),
                              target_tensor.view(-1))
                     print_vloss += vloss
-                    plot_vloss += vloss
 
             final_vloss = print_vloss / (i + 1)
+            print_vloss = 0
             print("avg val loss", final_vloss.item())
 
             if epoch == n_epochs:
@@ -113,7 +113,6 @@ class Training():
                 plot_train_losses.append(plot_loss_avg)
                 plot_val_losses.append(plot_vloss_avg.item())
                 plot_loss_total = 0
-                plot_vloss = 0
 
         print(plot_train_losses, plot_val_losses)
         self.showPlot(n_epochs, plot_train_losses, plot_val_losses, plot_name)